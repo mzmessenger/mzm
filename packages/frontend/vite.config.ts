93694daf44139bbb9780c612875cf96a72643d5e
Kohta Ito <kohta110@gmail.com>
2022-03-08 22:25:08 +0900

    add service worker (#29)

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,10 +1,32 @@
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'MZM',
+        short_name: 'MZM',
+        description: 'Chat system for developer',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#222429',
+        background_color: '#222429',
+        icons: [
+          {
+            src: '/mzm.png',
+            type: 'image/png',
+            sizes: '144x144'
+          }
+        ]
+      }
+    })
+  ],
   build: {
+    emptyOutDir: true,
     sourcemap: true,
     outDir: path.resolve(__dirname, 'dist')
   },
