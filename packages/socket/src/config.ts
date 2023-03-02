05e91f51eaa17fb242665284732a709c3e23afcd
Kohta Ito <kohta110@gmail.com>
2023-03-02 15:46:38 +0900

    fix options (#114)
    * add redis timeout
    
    * use watch option
    
    * bump version

--- packages/socket/src/config.ts
+++ packages/socket/src/config.ts
@@ -1,5 +1,8 @@
+import type { RedisOptions } from 'ioredis'
 import { config } from 'dotenv'
-config()
+if (process.env.NODE_ENV !== 'test') {
+  config()
+}
 
 export const WORKER_NUM = process.env.WORKER_NUM ?? 1
 
@@ -12,9 +15,11 @@ export const INTERNAL_API_URL = process.env.INTERNAL_API
 export const REDIS = {
   options: {
     host: process.env.REDIS_HOST,
-    enableOfflineQueue: false
-  }
-}
+    enableOfflineQueue: false,
+    connectTimeout: Number(process.env.SESSION_REDIS_TIMEOUT) ?? 30000
+  } satisfies RedisOptions
+} as const
+
 export const JWT = {
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
   internalAccessTokenSecret: process.env.INTERNAL_ACCESS_TOKEN_SECRET,
