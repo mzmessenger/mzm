d72da3d19d12ddf59e1a82b052450abb09fb920d
kohta ito <kohta110@gmail.com>
2023-03-06 15:08:11 +0900

    retry

--- packages/frontend/src/recoil/auth/hooks.tsx
+++ packages/frontend/src/recoil/auth/hooks.tsx
@@ -4,6 +4,7 @@ import { COOKIES } from 'mzm-shared/auth/constants'
 import { atom, useRecoilState, useRecoilValue } from 'recoil'
 import jwt_decode, { type JwtPayload } from 'jwt-decode'
 import dayjs from 'dayjs'
+import { sleep } from '../../lib/util'
 import { logger } from '../../lib/logger'
 
 const createDefaultToken = () => {
@@ -48,7 +49,8 @@ export const useAuth = () => {
   const refreshToken = async () => {
     type ResponseType = AUTH_API_RESPONSE['/auth/token/refresh']['POST']['body']
 
-    for (let i = 0; i < 3; i++) {
+    const MAX_RETRY_COUNT = 5
+    for (let i = 0; i < MAX_RETRY_COUNT; i++) {
       const res = await fetch('/auth/token/refresh', {
         credentials: 'include',
         method: 'POST'
@@ -59,8 +61,12 @@ export const useAuth = () => {
         setLoginFlag(true)
         return body
       }
+      if (i < MAX_RETRY_COUNT) {
+        await sleep(100)
+      }
     }
-    logout()
+    // @todo
+    console.warn('refresh token failed')
     return { accessToken: '', user: null }
   }
 
